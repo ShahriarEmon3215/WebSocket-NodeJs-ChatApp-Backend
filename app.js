3ab0ef8416b6f1f134d3e411769fc49cf4411c80
shahriaremon3215 <shahriar3215emon@gmail.com>
2023-12-31 17:47:32 +0600

    implemented pusher

--- app.js
+++ app.js
@@ -1,29 +1,58 @@
 const express = require("express");
+const router = require("express").Router();
 const socket = require("socket.io");
-
+var bodyParser = require("body-parser");
 const app = express();
 const PORT = process.env.PORT || 2023;
 
-const server = app.listen(PORT, () =>
-  console.log("Server is running at ", PORT)
-);
+app.use(bodyParser.json());
+// const server = app.listen(PORT, () =>
+//   console.log("Server is running at ", PORT)
+// );
 
-const io = socket(server);
+// const io = socket(server);
 
-const rooms = [1, 2, 3];
-io.on("connection", (socket) => {
-  console.log("Connected Socket.IO ", socket.id);
-  /// join room
-  socket.join(rooms[0]);
+// const rooms = [1, 2, 3];
+// io.on("connection", (socket) => {
+//   console.log("Connected Socket.IO ", socket.id);
+//   /// join room
+//   socket.join(rooms[0]);
 
-  /// message event
-  socket.on("message", (data) => {
-    /// emit message to "room1" and event "breadcast"
-    io.to(rooms[0]).emit("broadcast", data);
-  });
+//   /// message event
+//   socket.on("message", (data) => {
+//     /// emit message to "room1" and event "breadcast"
+//     io.to(rooms[0]).emit("broadcast", data);
+//   });
 
-  /// disconnect socket
-  socket.on("disconnect", () => {
-    console.log("Disconnected Socket.IO ");
-  });
+//   /// disconnect socket
+//   socket.on("disconnect", () => {
+//     console.log("Disconnected Socket.IO ");
+//   });
+// });
+
+var Pusher = require("pusher");
+
+var pusher = new Pusher({
+  appId: "1730116",
+  key: "a8c22b14a31ad1b20bb1",
+  secret: "01db3f5c64280d3bfe14",
+  cluster: "ap1",
 });
+
+pusher.trigger("chat-channel", "message-event", { message: "hello world" });
+
+router.post("/message", (req, res) => {
+  var body = req.body;
+
+  pusher.trigger("chat-channel", "message-event", {
+    user: body.user,
+    text: body.text,
+    time: body.time,
+  });
+  res.status(200).json({ message: "Message sent" });
+});
+
+
+app.use("/api", router);
+
+app.listen(PORT, () => console.log("Server is running at ", PORT));
